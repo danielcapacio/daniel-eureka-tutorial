a06aa66c43d887a4245cb43216bd41ce06d38067
Daniel Capacio <danielcapacio@gmail.com>
2017-10-23 00:54:44 -0700

    Display form data in new view

--- daniel-eureka-tutorial/ViewController.swift
+++ daniel-eureka-tutorial/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import Eureka
 
-class ViewController: UIViewController {
+class ViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // form ...
+        // Add Sections with +++
+        // Add Rows with <<<
+        form
+            +++ Section("A")
+                <<< TextRow() { row in
+                    row.title = "Text Row"
+                    row.placeholder = "Enter text here"
+                    row.tag = "Sample"
+                }
+                <<< PhoneRow() {
+                    $0.title = "Phone Row"
+                    $0.placeholder = "And numbers here"
+                }
+            +++ Section("B")
+                <<< DateRow() {
+                    $0.title = "Date Row"
+                    $0.value = Date(timeIntervalSinceReferenceDate: 0)
+                }
+                <<< ButtonRow("New View") { (row: ButtonRow) -> Void in
+                    row.title = row.tag
+                    row.presentationMode = .segueName(segueName: "showDataTransferController", onDismiss: nil)
+                }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDataTransferController" {
+            let destinationVC = segue.destination as! DataTransferController
+            // get text row input value
+            let row: TextRow? = form.rowBy(tag: "Sample")
+            let value = row?.value
+            destinationVC.data = value!
+        }
+    }
 }
 
